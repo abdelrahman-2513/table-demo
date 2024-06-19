751b4eee4bd921b170339f559560e91f17a54606
abdelrahman-2513 <114133265+abdelrahman-2513@users.noreply.github.com>
2024-06-19 14:49:11 +0300

    entire file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import TableComponent from './components/Table/Table';
-import CretePost from './components/CreatePost/CretePost';
+import { Tabs } from 'antd';
+
+// Components --------------------------------------------------------------------------------------------------------------------------
+import CreateProduct from './components/CreateProduct/createProduct';
+import DeleteProduct from './components/DeleteProduct/DeleteProduct';
+import UpdateProduct from './components/UpdateProduct/UpdateProduct';
 import Products from './components/Products/Products';
 
+
+const items = [{
+  key: 1,
+  label: "Products",
+  children: <Products />
+}, {
+  key: 2,
+  label: "Create",
+  children: <CreateProduct />
+}, {
+  key: 3,
+  label: "Update",
+  children: <UpdateProduct />
+}, {
+  key: 4,
+  label: "Delete",
+  children: <DeleteProduct />
+}]
 const App = () => {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient({
+
+  })
   return (
     <div className="app-container">
-      {/* <CretePost />
-      <TableComponent /> */}
-      <Products />
+
+      <Tabs defaultActiveKey="1" items={items} />
       <ReactQueryDevtools initialIsOpen={true} />
     </div>
   )
